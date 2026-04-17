6f80cde839735d76868451e04d16a0c950774911
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-04-17 12:16:36 +0200

    improve public api

--- analib.h
+++ analib.h
@@ -1,34 +1,37 @@
-// -------------- Details --------------
-//
-// Name        : Analib
-// Version     : 0.1.3
-//
-// Author      : Simon Danielsson
-// Email       : contact@simondanielsson.se
-// Repository  : https://github.com/simon-danielsson/analib-c
-// Website     : https://www.simondanielsson.se/
-//
-// ------------ MIT License ------------
-//
-// Copyright © 2026 Simon Danielsson
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files, to deal in the Software
-// without restriction, including without limitation the rights to use, copy,
-// modify, merge, publish, distribute, sublicense, and/or sell copies of the
-// Software, and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+/*
+
+-------------- Details --------------
+Name        : Analib
+Version     : 0.1.4
+
+Author      : Simon Danielsson
+Email       : contact@simondanielsson.se
+Repository  : https://github.com/simon-danielsson/analib-c
+Website     : https://www.simondanielsson.se/
+
+------------ MIT License ------------
+
+Copyright © 2026 Simon Danielsson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files, to deal in the Software
+without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
 
 #ifndef ANALIB_H_
 #define ANALIB_H_
@@ -42,67 +45,12 @@
 #define ANALIB_DEF
 #endif // ANALIB_DEF
 
-// DEFINITIONS: STRINGS
-// =============================================================================
-
-// get length of str
-ANALIB_DEF int AL_str_len(char *s);
-
-// DEFINITIONS: DEBUG
+// INTERNAL DEFINITIONS
 // =============================================================================
 
 // generic label builder for debug functions
-ANALIB_DEF static inline void AL_db_make_label(const char *label, char *header,
-                                               int header_size);
-
-// generic msg call for debug functions
-ANALIB_DEF static inline void AL_db_gen_msg(const char *type, const char *msg,
-                                            const char *file, int line,
-                                            const char *function);
-
-// formatted assert message that does not abort the program
-#define AL_db_assert(cond)                                                     \
-  if (cond == false) {                                                         \
-    AL_db_gen_msg("ASSERT", #cond, __FILE_NAME__, __LINE__, __FUNCTION__);     \
-  }                                                                            \
-  while (0)
-
-// formatted log message
-#define AL_db_log(msg)                                                         \
-  AL_db_gen_msg("LOG", msg, __FILE_NAME__, __LINE__, __FUNCTION__);
-
-// rust-like formatted todo message that aborts the program
-#define AL_db_todo(msg)                                                        \
-  do {                                                                         \
-    AL_db_gen_msg("TODO", (msg), __FILE_NAME__, __LINE__, __FUNCTION__);       \
-    abort();                                                                   \
-  } while (0)
-
-// DEFINITIONS: INTEGERS, FLOATS & DOUBLES
-// =============================================================================
-
-static inline int AL_min_int(int a, int b);
-static inline int AL_max_int(int a, int b);
-static inline double AL_min_double(double a, double b);
-static inline double AL_max_double(double a, double b);
-
-// compare two integers or doubles and return the smaller one
-#define AL_cmp_min(a, b)                                                       \
-  _Generic((a) + (b), int: min_int, double: min_double)(a, b)
-
-// compare two integers or doubles and return the bigger one
-#define AL_cmp_max(a, b)                                                       \
-  _Generic((a) + (b), int: max_int, double: max_double)(a, b)
-
-#endif // ANALIB_H_
-#ifdef ANALIB_IMPLEMENTATION
-
-// IMPLEMENTATIONS: DEBUG
-// =============================================================================
-
-// generic label builder for debug functions
-ANALIB_DEF static inline void AL_db_make_label(const char *label, char *header,
-                                               int header_size) {
+static inline void AL__intern_db_genlbl(const char *label, char *header,
+                                        int header_size) {
   int label_size = strlen(label);
   int wings_len = (header_size - label_size);
 
@@ -120,12 +68,12 @@ ANALIB_DEF static inline void AL_db_make_label(const char *label, char *header,
 }
 
 // generic msg call for debug functions
-ANALIB_DEF static inline void AL_db_gen_msg(const char *type, const char *msg,
-                                            const char *file, int line,
-                                            const char *function) {
+static inline void AL__intern_db_genmsg(const char *type, const char *msg,
+                                        const char *file, int line,
+                                        const char *function) {
 
   char label[9];
-  AL_db_make_label(type, label, 9);
+  AL__intern_db_genlbl(type, label, 9);
 
   char space[11 + 1];
   memset(space, ' ', 11);
@@ -134,20 +82,56 @@ ANALIB_DEF static inline void AL_db_gen_msg(const char *type, const char *msg,
   fprintf(stderr, "\n->%s  %s:%d (%s)\n%s %s\n", label, file, line, function,
           space, msg);
 }
-// IMPLEMENTATIONS: INTEGERS
-// =============================================================================
 
-ANALIB_DEF static inline int min_int(int a, int b) { return a < b ? a : b; }
-ANALIB_DEF static inline int max_int(int a, int b) { return a > b ? a : b; }
-ANALIB_DEF static inline double min_double(double a, double b) {
+static inline int AL__intern_min_int(int a, int b) { return a < b ? a : b; }
+static inline int AL__intern_max_int(int a, int b) { return a > b ? a : b; }
+static inline double AL__intern_min_double(double a, double b) {
   return a < b ? a : b;
 }
-ANALIB_DEF static inline double max_double(double a, double b) {
+static inline double AL__intern_max_double(double a, double b) {
   return a > b ? a : b;
 }
 
-// IMPLEMENTATIONS: STRINGS
+// PUBLIC API
 // =============================================================================
+
+// get length of str
+ANALIB_DEF int AL_str_len(char *s);
+
+// formatted assert message that does not abort the program
+#define AL_db_assert(cond)                                                     \
+  if (cond == false) {                                                         \
+    AL__intern_db_genmsg("ASSERT", #cond, __FILE_NAME__, __LINE__, __func__);  \
+  }                                                                            \
+  while (0)
+
+// formatted log message
+#define AL_db_log(msg)                                                         \
+  AL__intern_db_genmsg("LOG", msg, __FILE_NAME__, __LINE__, __func__);
+
+// rust-like formatted todo message that aborts the program
+#define AL_db_todo(msg)                                                        \
+  do {                                                                         \
+    AL__intern_db_genmsg("TODO", (msg), __FILE_NAME__, __LINE__, __func__);    \
+    abort();                                                                   \
+  } while (0)
+
+// compare two integers or doubles and return the smaller one
+#define AL_cmp_min(a, b)                                                       \
+  _Generic((a) + (b), int: AL__intern_min_int, double: AL__intern_min_double)( \
+      a, b)
+
+// compare two integers or doubles and return the bigger one
+#define AL_cmp_max(a, b)                                                       \
+  _Generic((a) + (b), int: AL__intern_max_int, double: AL__intern_max_double)( \
+      a, b)
+
+#endif // ANALIB_H_
+
+// IMPLEMENTATION
+// =============================================================================
+
+#ifdef ANALIB_IMPLEMENTATION
 
 ANALIB_DEF int AL_str_len(char *s) {
   char *t;
--- tests/test.c
+++ tests/test.c
@@ -9,13 +9,16 @@ int sum(int x, int y) {
 
 int main(void) {
 
-    int z = sum(5, 4);
-    // printf("%d\n", AL_cmp_min(5, 6));
-    // printf("%.2f\n", AL_cmp_min(5.2, 6.8));
-    // printf("%d\n", AL_cmp_max(5, 6));
-    // printf("%.2f\n", AL_cmp_max(5.2, 6.8));
-    AL_db_assert(5 == 6);
-    AL_db_log("This is something I want to log");
-    AL_db_todo("this is yet to be done");
+    // int z = sum(5, 4);
+
+    printf("%d\n", AL_cmp_min(5, 6));
+    printf("%.2f\n", AL_cmp_min(5.2, 6.8));
+    printf("%d\n", AL_cmp_max(5, 6));
+    printf("%.2f\n", AL_cmp_max(5.2, 6.8));
+    printf("%.2f\n", AL_cmp_max(5.2, 6.8));
+    AL__intern_db_genmsg("WARNING", "this is a warning", __FILE_NAME__, __LINE__,
+            __func__);
+    // AL_db_log("This is something I want to log");
+    // AL_db_todo("this is yet to be done");
     return 0;
 }
